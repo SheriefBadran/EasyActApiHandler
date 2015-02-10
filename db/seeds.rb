ec0cc648f20cfe5a381db7d518751554b7c32e45
SheriefBadran <sb222rf@student.lnu.se>
2015-02-10 10:28:30 +0100

    updated seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@
 # test@test.se: testpassword
 # admin@test.se: adminpassword
 
-user = User.create({
+user = User.create!({
                        id: 1, email: 'test@test.se',
-                       encrypted_password: '$2a$10$KLRBIoQqWuDsB5Sb58cUrOOFRrrTyBotLrpY3eh16zB86HHVqtwfG',
+                       password: 'testpassword',
                        api_key: 'da3bebae-b6a1-4333-9835-b4d94387b1b1',
                        admin: 'false'
                    })
 
-admin = User.create({
+admin = User.create!({
                         id: 2, email: 'admin@test.se',
-                        encrypted_password: '$2a$10$EY.g4oYqcuYetmRncNMBnemoixbHY0SJ2Jy.ZZVWo8.L7WMxB8wjK',
+                        password: 'adminpassword',
                         api_key: '9864e2f2-212c-4984-8580-fdd6f6a00b95',
                         admin: 'true'
                     })
